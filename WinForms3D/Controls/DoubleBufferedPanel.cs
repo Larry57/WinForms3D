ef756295bf9f6af2c9e0858e35b34c668bcf6f10
L4rry <laurent.bourgeois@outlook.com>
2020-02-08 15:13:55 +0100

    Added DoubleBufferedPanel

---
+++ WinForms3D/Controls/DoubleBufferedPanel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms3D {
+    public class DoubleBufferedPanel : Panel {
+        public DoubleBufferedPanel() => this.DoubleBuffered = true;
+    }
+}
